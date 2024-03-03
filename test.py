fc0070f905f181fbb379f28443760ca5fe07c714
Willow94 <161991259+Willow94@users.noreply.github.com>
2024-03-03 10:23:55 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+# pemjumlahan
+x = 9
+y = 6 
+print("jumlah x+y =", x+y)
+
+# akar kuadrat
+import math
+
+x1 = 16
+# Menghitung akar kuadrat
+akar_kuadrat = math.sqrt(x1)
+
+print("Akar kuadrat dari", x1, "adalah", akar_kuadrat)
+
+
